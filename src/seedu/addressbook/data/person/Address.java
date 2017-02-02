aae977238f7fd47cf1efde10688e4e34050b6907
CHQ\Ching <hqching@gmail.com>
2017-02-03 06:34:02 +0800

    implement classes within address class

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -1,8 +1,5 @@
 package seedu.addressbook.data.person;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import seedu.addressbook.data.exception.IllegalValueException;
 
 /**
@@ -11,12 +8,16 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
+    /**
+     * Represents an address block. Guarantees: immutable; is
+     * valid as declared in {@link #isValidBlock(String)}
+     */
     private class Block {
         public static final String EXAMPLE = "123";
-        public static final String MESSAGE_BLOCK_CONSTRAINTS = "Address blocks should only have alphanumeric characters";
-        public static final String BLOCK_VALIDATION_REGEX = ".+";
+        public static final String MESSAGE_BLOCK_CONSTRAINTS = "Address blocks should be only numbers with an optional letter at the end";
+        public static final String BLOCK_VALIDATION_REGEX = "\\d+[a-zA-Z]?";
         private String number;
-
+        
         public Block(String blkNum) throws IllegalValueException {
             if (!this.isValidBlock(blkNum)) {
                 throw new IllegalValueException(MESSAGE_BLOCK_CONSTRAINTS);
@@ -34,6 +35,10 @@ public class Address {
 
     }
 
+    /**
+     * Represents an address street. Guarantees: immutable; 
+     * is valid as declared in {@link #isValidStreet(String)}
+     */
     private class Street {
 
         public static final String EXAMPLE = "Clementi Ave 3";
@@ -57,6 +62,10 @@ public class Address {
         }
     }
 
+    /**
+     * Represents an address unit. Guarantees: immutable; 
+     * is valid as declared in {@link #isValidUnit(String)}
+     */
     private class Unit {
 
         public static final String EXAMPLE = "#12-34";
@@ -80,6 +89,10 @@ public class Address {
         }
     }
 
+    /**
+     * Represents an address postal code. Guarantees: immutable; 
+     * is valid as declared in {@link #isValidPostalCode(String)}
+     */
     private class PostalCode {
 
         public static final String EXAMPLE = "231534";
@@ -103,16 +116,16 @@ public class Address {
         }
     }
 
-    public static final String EXAMPLE = Block.EXAMPLE + ", " + Street.EXAMPLE + ", " + Unit.EXAMPLE + ","
-            + PostalCode.EXAMPLE;
+    public static final String ADDRESS_DELIMITER = ",";
+    public static final String EXAMPLE = Block.EXAMPLE + ADDRESS_DELIMITER + Street.EXAMPLE + ADDRESS_DELIMITER
+                                       + Unit.EXAMPLE + ADDRESS_DELIMITER + PostalCode.EXAMPLE;
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses must have format BLOCK, STREET, UNIT, POSTAL_CODE";
-    public static final Pattern ADDRESS_VALIDATION_REGEX = // ", " comma and
-                                                           // whitespace is
-                                                           // reserved for
-                                                           // delimiter prefixes
-            Pattern.compile(
-                    "(?<BLOCK>.+)" + ", " + "(?<STREET>.+)" + ", " + "(?<UNIT>.+)" + ", " + "(?<POSTALCODE>.+)");
-
+    
+    private static final int ADDRESS_NUMBER_OF_PARAMS = 4;
+    private static final int ADDRESS_BLOCK_INDEX = 0;
+    private static final int ADDRESS_STREET_INDEX = 1;
+    private static final int ADDRESS_UNIT_INDEX = 2;
+    private static final int ADDRESS_POSTAL_CODE_INDEX = 3;
     private Block block;
     private Street street;
     private Unit unit;
@@ -125,28 +138,37 @@ public class Address {
      * @throws IllegalValueException if given address string is invalid.
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
-        String trimmedAddress = address.trim();
-        if (!isValidAddress(trimmedAddress)) {
+        String[] addressParams = address.trim().split(ADDRESS_DELIMITER);
+        if (!isValidAddress(addressParams)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.isPrivate = isPrivate;
-        final Matcher matcher = ADDRESS_VALIDATION_REGEX.matcher(address.trim());
-        block = new Block(matcher.group("BLOCK"));
-        street = new Street(matcher.group("STREET"));
-        unit = new Unit(matcher.group("UNIT"));
-        postalCode = new PostalCode(matcher.group("POSTALCODE"));
+        setAddressParams(isPrivate, addressParams);
     }
 
     /**
      * Returns true if a given string is a valid person address.
      */
-    public static boolean isValidAddress(String test) {
-        return ADDRESS_VALIDATION_REGEX.matcher(test).matches();
+    public static boolean isValidAddress(String[] test) {        
+        return (test.length == ADDRESS_NUMBER_OF_PARAMS);       
     }
+    
+    /**
+     * Splits address into block, street, unit and postal code 
+     * 
+     * @throws IllegalValueException if any of the above have invalid format
+     */
+    private void setAddressParams(boolean isPrivate, String[] addressParams) throws IllegalValueException {
+        this.isPrivate = isPrivate;  
+        block = new Block(addressParams[ADDRESS_BLOCK_INDEX].trim());
+        street = new Street(addressParams[ADDRESS_STREET_INDEX].trim());
+        unit = new Unit(addressParams[ADDRESS_UNIT_INDEX].trim());
+        postalCode = new PostalCode(addressParams[ADDRESS_POSTAL_CODE_INDEX].trim());
+    }
+
 
     @Override
     public String toString() {
-        return block.getBlock() + ", " + street.getStreet() + ", " + unit.getUnit() + ", " + postalCode.getPostalCode();
+        return block.getBlock() + ADDRESS_DELIMITER + street.getStreet() + ADDRESS_DELIMITER + unit.getUnit() + ADDRESS_DELIMITER + postalCode.getPostalCode();
     }
 
     @Override
