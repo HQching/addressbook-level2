a9d33e98a65e787a7b781a989cb5ce9415c099c2
CHQ\Ching <hqching@gmail.com>
2017-02-08 02:26:38 +0800

    header coment for interface

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,5 +1,6 @@
 package seedu.addressbook.data.person;
 
+/** Printable interface for returning a formatted String of a Person's contact details.  */
 public interface Printable {
     
     String getPrintableString();
