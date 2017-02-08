0f2fbdc8e9ab14ed12fedfc1a94971fb2f47251f
CHQ\Ching <hqching@gmail.com>
2017-02-08 19:08:40 +0800

    test a few more scenarios in UtilsTest using JUnit

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -3,6 +3,7 @@ package seedu.addressbook.common;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,11 +15,26 @@ public class UtilsTest {
         // empty list
         assertFalse(Utils.isAnyNull());
 
-        // Any non-empty list
-        assertFalse(Utils.isAnyNull(new Object(), new Object()));
+        // non empty list with one non-null argument
+        assertFalse(Utils.isAnyNull(new ArrayList<Object>()));
+        assertFalse(Utils.isAnyNull(new Object()));
         assertFalse(Utils.isAnyNull("test"));
+        assertFalse(Utils.isAnyNull(1));
         assertFalse(Utils.isAnyNull(""));
-
+        
+        // non empty list with more than one non-null arguments
+        assertFalse(Utils.isAnyNull(new Object(), new Object()));
+        assertFalse(Utils.isAnyNull("item", "item", "item"));
+        assertFalse(Utils.isAnyNull(1,2,3,4,5,6,7,8,9,10));
+        assertFalse(Utils.isAnyNull(" ", "\n", "", 23, "string", new Object()));
+        
+        // non empty list with same objects as arguments
+        Object a = new Object();
+        Object b = a;
+        assertFalse(Utils.isAnyNull(a,b));
+        a = null;
+        assertFalse(Utils.isAnyNull(b));
+        
         // non empty list with just one null at the beginning
         assertTrue(Utils.isAnyNull((Object) null));
         assertTrue(Utils.isAnyNull(null, "", new Object()));
@@ -32,9 +48,14 @@ public class UtilsTest {
         assertTrue(Utils.isAnyNull("", new Object(), null));
         assertTrue(Utils.isAnyNull(new Object(), new Object(), null));
 
+        // non empty list with nulls all over the list
+        assertTrue(Utils.isAnyNull("", null, 78, "string", null, new Object(), null));
+        
         // confirms nulls inside the list are not considered
         List<Object> nullList = Arrays.asList((Object) null);
         assertFalse(Utils.isAnyNull(nullList));
+        assertTrue(Utils.isAnyNull(nullList.get(0)));
+        
     }
 
     @Test
@@ -51,7 +72,9 @@ public class UtilsTest {
         // all objects unique
         assertAreUnique("abc", "ab", "a");
         assertAreUnique(1, 2);
-
+        assertAreUnique(new Object(), new Object(), new Object());
+        assertAreUnique(1,"abc", null, new Object());
+        
         // some identical objects
         assertNotUnique("abc", "abc");
         assertNotUnique("abc", "", "abc", "ABC");
@@ -60,7 +83,20 @@ public class UtilsTest {
         assertNotUnique(null, 1, new Integer(1));
         assertNotUnique(null, null);
         assertNotUnique(null, "a", "b", null);
+        
+        //testing using arrays
+        int[] arr1 = { 1, 3, 5, 7, 9};
+        int [] arr2 = arr1;
+        int [] arr3 = {2, 4, 6, 8, 10};
+        assertAreUnique(arr1, arr3);
+        assertNotUnique(arr1, arr2);
+        arr2[0] = 2;
+        assertNotUnique(arr1, arr2);
+        arr1 = null;
+        assertAreUnique(arr1, arr2);
+
     }
+    
 
     private void assertAreUnique(Object... objects) {
         assertTrue(Utils.elementsAreUnique(Arrays.asList(objects)));
